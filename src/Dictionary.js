6bcf0982cf8053663950bd0932cee6f7f13f8446
Gabriela85 <77332290+Gabriela85@users.noreply.github.com>
2021-11-24 22:46:19 +0100

    setup of definitions and meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
+
 
 export default function Dictionary () {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response){
-      console.log(response.data[0]);  
+   setResults(response.data[0]);
     }
 
     function search(event){
@@ -30,6 +33,9 @@ onChange={handleKeywordChange}
 placeholder="Type your word..." />
 <input type="submit" className="submit" value="Search" />
 </form>
+
+<Results results={results} />
+
 </div>
     );
 }
