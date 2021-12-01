0ce3c0c96fc70e8907d7b833ef4f20c95cb1d5d0
Gabriela85 <77332290+Gabriela85@users.noreply.github.com>
2021-12-01 13:03:14 +0100

    add the photos of searched pics

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,24 +2,38 @@ import React, { useState } from 'react';
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
+
 
 
 export default function Dictionary () {
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response){
+    function handleDictionResponse(response){
    setResults(response.data[0]);
     }
+
+    function handlePexelsResponse(response){
+        setPhotos(response.data.photos);
+         }
 
     function search(event){
     event.preventDefault();
     
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-axios.get(apiUrl).then(handleResponse);
-}
+axios.get(apiUrl).then(handleDictionResponse);
 
-    
+let pexelsApiKey = "563492ad6f917000010000013c47a2d4108b4df7962a590629b7914f";
+let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=3`;
+let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+axios
+.get(pexelsApiUrl, { headers: headers }).then
+(handlePexelsResponse);
+    }   
+
+
    function handleKeywordChange(event){
    setKeyword(event.target.value);
    } 
@@ -35,6 +49,7 @@ placeholder="Type your word..." />
 </form>
 
 <Results results={results} />
+<Photos photos = {photos} />
 
 </div>
     );
